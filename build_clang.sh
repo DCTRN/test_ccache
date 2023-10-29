5669624f60da961701cb2add6418c46d2a549599
DCTRN <michal.pytlik98@gmail.com>
2023-10-29 16:12:13 +0000

    ConcreteHandler1.cpp increase to 200

--- build_clang.sh
+++ build_clang.sh
@@ -3,8 +3,8 @@
 export CC=/usr/local/bin/clang
 export CXX=/usr/local/bin/clang++
 
-export CCACHE_LOGFILE=/workspace/test_ccache_1/ccache/logs.txt
-export CCACHE_DIR=/workspace/test_ccache_1/ccache
+export CCACHE_LOGFILE=/workspace/test_ccache_2/ccache/logs.txt
+export CCACHE_DIR=/workspace/test_ccache_2/ccache
 
 cmake -G Ninja -S . -B build
 time cmake --build build | tee time_measurements.txt
