a42513fe62a675a8a9e102632d686d6307e3653c
DCTRN <michal.pytlik98@gmail.com>
2023-10-29 16:01:19 +0000

    Linux modifications

--- build_clang.sh
+++ build_clang.sh
@@ -3,7 +3,8 @@
 export CC=/usr/local/bin/clang
 export CXX=/usr/local/bin/clang++
 
-export CCACHE_LOGFILE=/workspace/ccache/logs.txt
+export CCACHE_LOGFILE=/workspace/test_ccache_1/ccache/logs.txt
+export CCACHE_DIR=/workspace/test_ccache_1/ccache
 
 cmake -G Ninja -S . -B build
 time cmake --build build | tee time_measurements.txt
